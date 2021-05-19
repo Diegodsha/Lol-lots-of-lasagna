08dd8fd21175092b7a4d96fd4842263dc5f25ee9
Diego <diegodsha@hotmail.com>
2021-05-19 16:45:04 -0500

    Built home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import './style.scss';
-import icon1 from './assets/mainbackmin.png';
-import icon2 from './assets/bkcode6.jpg';
 
 // alert('restaurant page');
-const cont = document.getElementById('content');
-const myIcon = new Image();
-
-myIcon.src = icon1;
 
 // cont.appendChild(myIcon);
