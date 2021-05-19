dc3d72bb3bf31142095fc32a533338eb4c4cf7b1
Diego <diegodsha@hotmail.com>
2021-05-19 12:51:47 -0500

    Initial configuration of webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // {
+      //   test: /\.(jpe?g|png)$/i,
+      //   use: ['file-loader', 'webp-loader'],
+      // },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 5000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
